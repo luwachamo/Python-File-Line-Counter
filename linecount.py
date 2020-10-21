5fa41bacf08c1e38c6a7ac26ac9caf672a7149de
luwachamo <12202111+luwachamo@users.noreply.github.com>
2020-10-21 16:44:55 -0700

    Add files via upload

---
+++ linecount.py
@@ -0,0 +1,43 @@
+def line_counter(file_name):
+    with open(get, 'r') as file:
+        count = 0
+        large_comment = False
+        for line in file:
+            line = line.strip()
+            line.replace(" ", "")
+            if line == "":
+                continue
+            elif line[0] == '#':
+                continue
+
+            elif "'''" in line:
+                back_quote = line.find(r'\"') + 1
+                trip_quote = line.find("'''")
+                quote = line.find('"')
+
+                pass1 = back_quote != -1 and quote == back_quote
+                pass2 = quote != -1 and quote < trip_quote
+
+                if not pass1 and not pass2:
+                    if large_comment == True:
+                        large_comment = False
+                    elif large_comment == False:
+                        large_comment = True
+                    print(large_comment)
+                    continue
+
+            if large_comment == False:
+                count += 1
+    return(count)
+
+
+
+try:
+    print("Simple line counter function that ignores:\n")
+    print("'''\ntriple\nquote\nblocks\n'''")
+    print("#single line comments\n")
+    print("\nand whitespace.\n")
+    get = input("Type python file name: ")
+    print(line_counter(get))
+except FileNotFoundError:
+    print("Couldn't find file: \"{}\"".format(get))
