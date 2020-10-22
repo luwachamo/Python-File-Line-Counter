f37008afe9d73aa863d854de51fec699ee06afad
luwachamo <12202111+luwachamo@users.noreply.github.com>
2020-10-21 17:05:06 -0700

    Create pycount.py
    A cli version of the line counter.
    Just type: python(or python3) pycount.py FILENAMES
    to use it. Accepts multiple files.

---
+++ pycount.py
@@ -0,0 +1,40 @@
+import sys
+
+def line_counter(file_name):
+    with open(file_name, 'r') as file:
+        count = 0
+        large_comment = False
+        for line in file:
+            line = line.strip()
+            line.replace(" ", "")
+            if line == "":
+                continue
+            elif line[0] == '#':
+                continue
+
+            elif "'''" in line:
+                back_quote = line.find(r'\"') + 1
+                trip_quote = line.find("'''")
+                quote = line.find('"')
+
+                pass1 = back_quote != -1 and quote == back_quote
+                pass2 = quote != -1 and quote < trip_quote
+
+                if not pass1 and not pass2:
+                    if large_comment == True:
+                        large_comment = False
+                    elif large_comment == False:
+                        large_comment = True
+                    continue
+
+            if large_comment == False:
+                count += 1
+    return(count)
+
+args = sys.argv
+
+for i in range(1,len(args)):
+    try:
+        print("{} has {} lines!".format(args[i], line_counter(args[i])))
+    except FileNotFoundError:
+        print("Couldn't find file: \"{}\"".format(args[i]))
